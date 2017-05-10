6e232c9d335361c16e4f87349ae2a85709a3af5b
Srinivas V <srinivasv147@gmail.com>
2017-05-10 15:38:52 +0530

    added set operations

--- set.js
+++ set.js
@@ -1,6 +1,8 @@
 // here I have implemented a set in java script.
 // the functions present in the set are given elow.
 // add, delete, has, clear, size, values.
+// the operations implemented here are.
+// union, intersection, difference, subset.
 // I have used an object to represent the set as in ds algo book.
 
 var Set = function(){
@@ -49,11 +51,61 @@ var Set = function(){
     }
     return temp;
   }
+
+  this.union = function(set2){
+    var set3 = new Set();
+    for(var1 in this.items){
+      set3.add(var1);
+    }
+    for(var2 in set2.items){
+      set3.add(var2);
+    }
+    return set3;
+  }
+
+  this.intersection = function(set2){
+    var set3 = new Set();
+    for(var1 in this.items){
+      if(var1 in set2.items){
+        set3.add(var1);
+      }
+    }
+    return set3;
+  }
+
+  this.difference = function(set2){
+    var set3 = new Set();
+    for(var1 in this.items){
+      if(!(var1 in set2.items)){
+        set3.add(var1);
+      }
+    }
+    return set3;
+  }
+
+  this.subset = function(set2){
+    //this checks if this is a subset of set2
+    var temp = true;
+    for(var1 in this.items){
+      if(!(var1 in set2.items)){
+        temp = !temp;
+      }
+    }
+    return temp;
+  }
 }
 
-var set = new Set();
-set.add(1);set.add(2);set.delete(2);console.log(set.has(1),set.has(2));
-console.log(set.values());
-console.log(set.size());
-set.clear();
-console.log(set.values());
+// var set = new Set();
+// set.add(1);set.add(2);set.delete(2);console.log(set.has(1),set.has(2));
+// console.log(set.values());
+// console.log(set.size());
+// set.clear();
+// console.log(set.values());
+var set1 = new Set();
+var set2 = new Set();
+set1.add(1);set1.add(2);set2.add(1);set2.add(3);set2.add(4);set1.delete(2);
+console.log("union",set1.union(set2).values())
+console.log("intersection",set1.intersection(set2).values());
+console.log("difference",set1.difference(set2).values());
+console.log("subset",set1.subset(set2));
+// console.log(set3.values());
